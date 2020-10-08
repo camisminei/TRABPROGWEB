6a25d72b5f1a4e0e68c9f76c4893fabf311956af
Camila Minei <camisminei.ufms@gmail.com>
2020-10-08 11:19:33 -0400

    adicao de card na coluna

---
+++ js/script.js
@@ -0,0 +1,15 @@
+class AdicaoCard{
+
+    addCard(){
+        var novoCard = document.createElement("div");
+        let imgExcluir = document.createElement("img");
+        let imgEditar = document.createElement("img");
+        imgExcluir.src = "img/trash.svg";
+        imgEditar.src = "img/pencil.svg";
+        novoCard.className="card-col";
+        novoCard.appendChild(imgEditar);
+        novoCard.appendChild(imgExcluir);
+        document.getElementById("card-col").appendChild(novoCard);
+    }
+}
+let add = new AdicaoCard();
