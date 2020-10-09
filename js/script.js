f429b1a42a540c253c4f2e94618f425f9cbf1e2b
Camila Minei <camisminei.ufms@gmail.com>
2020-10-09 08:37:50 -0400

    correçao concluida do check

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ class AdicaoCard{
         var novoCard2 = document.createElement("div");
 
         let check = document.createElement("img");
-        let check2 = document.createElement("img");
         let imgExcluir = document.createElement("img");
         let imgEditar = document.createElement("img");
 
@@ -14,11 +13,14 @@ class AdicaoCard{
 
         check.src = "img/circle-outline.png";
         check.addEventListener("click", function(){
-            check.remove();
-            check2.src = "img/check.png";
-        })
-        check2.addEventListener("click", function(){
-            check.src = "img/circle-outline.png";
+
+            if(check.getAttribute("src") === "img/circle-outline.png"){
+                check.setAttribute("src","img/check.png");
+            }
+            else{
+                check.setAttribute("src","img/circle-outline.png");
+            }
+            
         })
         
         novoCard.className="card-col";
@@ -26,17 +28,12 @@ class AdicaoCard{
 
 
         novoCard2.appendChild(check);
-        novoCard2.appendChild(check2);
         document.getElementById("card-col").appendChild(novoCard2);
         
 
         novoCard.appendChild(imgEditar);
         novoCard.appendChild(imgExcluir);
         document.getElementById("card-col").appendChild(novoCard);
-    }
-
-    changeImg(){
-
     }
 }
 let add = new AdicaoCard();
