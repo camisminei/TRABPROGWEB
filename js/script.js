bddbe96b11daa54fda3698cd5177613d4f2185dd
Luciano Esquer <lucianoesquer9944@gmail.com>
2020-10-10 19:45:04 -0400

    alert mensagem

--- js/script.js
+++ js/script.js
@@ -32,7 +32,21 @@ class AdicaoCard{
 
         novoCard.appendChild(imgEditar);
         novoCard.appendChild(imgExcluir);
+
         document.getElementById("card-col").appendChild(novoCard);
+        imgExcluir.setAttribute("onclick",`add.excluir_tela()`);
+
     }
+    excluir_tela(){
+        alert("clique em 'ok' para excluir");
+       //let novadiv = document.createElement("div");
+
+       // document.getElementById("divexcluir").innerHTML = '<div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">tarefa x</h5><button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><p>deseja deletar essa tarefa?</p></div><div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal">Excluir</button><button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button></div></div></div></div>';
+       // document.getElementById("divexcluir").appendChild(novadiv);
+        //alert("teste2");
+
+    }
+
+
 }
 let add = new AdicaoCard();
